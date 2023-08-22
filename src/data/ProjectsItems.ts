4a3d38fd7ec28374fba6d23118bc66af7b4576fa
diegoOG09 <xdiegoemmax@outlook.com>
2023-08-21 20:34:43 -0600

    Finish projects section

--- src/data/ProjectsItems.ts
+++ src/data/ProjectsItems.ts
@@ -6,14 +6,14 @@ const projectsItems : ProjectItem[] = [
     title: 'Algorithmics Landing Page',
     description: 'Landing page for Algorithmics. Users will find information about the school and available courses.',
     repoUrl: 'https://github.com/diegoOG09/algorithmics-landing-web',
-    websiteUrl: 'https://genuine-trifle-48300e.netlify.app/'
+    websiteUrl: 'https://algorithmics-landing-page.netlify.app'
   },
   {
     image: '/assets/screenshots/algo-lessons-website.png',
     title: 'Algorithmics Sample Classes',
     description: 'Algorithmics website to schedule sample classes. The child(\')s parents can schedule a sample class to meet the teachers and the course.',
     repoUrl: 'https://github.com/diegoOG09/algorithmics-landing-page',
-    websiteUrl: 'https://deluxe-parfait-f5ff57.netlify.app/'
+    websiteUrl: 'https://algorithmics-sample-classes.netlify.app'
   },
   {
     image: '/assets/screenshots/analyze-this.png',
@@ -21,6 +21,13 @@ const projectsItems : ProjectItem[] = [
     description: 'Web tool for analyze text, number of words, palindromes, etc. ',
     repoUrl: 'https://github.com/diegoOG09/analyze-this',
     websiteUrl: 'https://analyze-this.netlify.app/'
+  },
+  {
+    image: '/assets/screenshots/viajes-bernal.png',
+    title: 'Viajes Bernal',
+    description: 'Travel website where users can view upcoming and past trips. It also has an administration panel for those administrators who are logged in. They can create, delete and modify trips.',
+    repoUrl: 'https://github.com/diegoOG09/travels',
+    websiteUrl: 'https://viajes-bernal.netlify.app/'
   }
 ]
 

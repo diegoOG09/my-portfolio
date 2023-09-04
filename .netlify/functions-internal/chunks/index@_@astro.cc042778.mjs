f05ec1714e69b267ee42389cc3c22b17a63a6e1f
diegoOG09 <xdiegoemmax@outlook.com>
2023-09-04 08:32:54 -0600

    Add netlify folder

---
+++ .netlify/functions-internal/chunks/index@_@astro.cc042778.mjs
@@ -0,0 +1,14 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.80ce4722.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/index.astro.f90744d1.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/entry.mjs
@@ -0,0 +1,27 @@
+import * as adapter from '@astrojs/netlify/netlify-functions.js';
+import { renderers } from './renderers.mjs';
+import 'mime';
+import 'cookie';
+import 'kleur/colors';
+import { g as deserializeManifest } from './chunks/astro.80ce4722.mjs';
+import '@astrojs/internal-helpers/path';
+import 'html-escaper';
+import 'path-to-regexp';
+import 'string-width';
+
+const _page0  = () => import('./chunks/index@_@astro.cc042778.mjs');const pageMap = new Map([["src/pages/index.astro", _page0]]);
+const _manifest = Object.assign(deserializeManifest({"adapterName":"@astrojs/netlify/functions","routes":[{"file":"","links":[],"scripts":[],"styles":[{"type":"external","src":"/_astro/index.ef4e7b22.css"}],"routeData":{"route":"/","type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"_meta":{"trailingSlash":"ignore"}}}],"base":"/","compressHTML":false,"markdown":{"drafts":false,"syntaxHighlight":"shiki","shikiConfig":{"langs":[],"theme":"github-dark","wrap":false},"remarkPlugins":[],"rehypePlugins":[],"remarkRehype":{},"gfm":true,"smartypants":true},"componentMetadata":[["C:/Users/xdieg/my-portfolio/src/pages/index.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"clientDirectives":[["idle","(()=>{var i=t=>{let e=async()=>{await(await t())()};\"requestIdleCallback\"in window?window.requestIdleCallback(e):setTimeout(e,200)};(self.Astro||(self.Astro={})).idle=i;window.dispatchEvent(new Event(\"astro:idle\"));})();"],["load","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event(\"astro:load\"));})();"],["media","(()=>{var s=(i,t)=>{let a=async()=>{await(await i())()};if(t.value){let e=matchMedia(t.value);e.matches?a():e.addEventListener(\"change\",a,{once:!0})}};(self.Astro||(self.Astro={})).media=s;window.dispatchEvent(new Event(\"astro:media\"));})();"],["only","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event(\"astro:only\"));})();"],["visible","(()=>{var r=(i,c,n)=>{let s=async()=>{await(await i())()},t=new IntersectionObserver(e=>{for(let o of e)if(o.isIntersecting){t.disconnect(),s();break}});for(let e of n.children)t.observe(e)};(self.Astro||(self.Astro={})).visible=r;window.dispatchEvent(new Event(\"astro:visible\"));})();"]],"entryModules":{"\u0000@astrojs-ssr-virtual-entry":"_@astrojs-ssr-virtual-entry.mjs","\u0000@astro-renderers":"renderers.mjs","\u0000empty-middleware":"_empty-middleware.mjs","/src/pages/index.astro":"chunks/pages/index.astro.f90744d1.mjs","\u0000@astro-page:src/pages/index@_@astro":"chunks/index@_@astro.cc042778.mjs","astro:scripts/before-hydration.js":""},"assets":["/_astro/ProductSans-Regular.744c8a73.ttf","/_astro/index.ef4e7b22.css","/favicon.svg","/docs/resume.pdf","/assets/icons/arrow-down.svg","/assets/icons/css.svg","/assets/icons/firebase.svg","/assets/icons/html.svg","/assets/icons/javascript.svg","/assets/icons/python.svg","/assets/icons/rocket-2.svg","/assets/icons/rocket-exp.svg","/assets/icons/rocket.svg","/assets/icons/sass.svg","/assets/icons/sql.svg","/assets/icons/telescope-education.svg","/assets/icons/vue.svg","/assets/screenshots/algo-landing-page-mobile.png","/assets/screenshots/algo-landing-page-mobile2.png","/assets/screenshots/algo-landing-page.png","/assets/screenshots/algo-lessons-website.png","/assets/screenshots/analyze-this.png","/assets/screenshots/viajes-bernal.png"]}), {
+	pageMap,
+	renderers,
+});
+const _args = {};
+
+const _exports = adapter.createExports(_manifest, _args);
+const handler = _exports['handler'];
+
+const _start = 'start';
+if(_start in adapter) {
+	adapter[_start](_manifest, _args);
+}
+
+export { handler, pageMap };

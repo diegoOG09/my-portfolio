7ce4d8505ad960373993c97b124a93f000faf371
diegoOG09 <xdiegoemmax@outlook.com>
2024-03-11 22:02:06 -0600

    Update

--- src/data/EducationItems.ts
+++ src/data/EducationItems.ts
@@ -8,12 +8,6 @@ const educationItems : EducationItem[] = [
     date: '2021-2023'
   },
   {
-    title: 'Instituto Tecnológico de Toluca',
-    career: 'Computer Systems Engineering',
-    location: 'Metepec, Estado de México',
-    date: '2017-2020'
-  },
-  {
     title: 'Scrum Fundamentals Certified',
     career: 'SCRUMstudy - Accreditation Body for Scrum and Agile',
     location: 'México',
--- src/data/ProjectsItems.ts
+++ src/data/ProjectsItems.ts
@@ -28,6 +28,20 @@ const projectsItems : ProjectItem[] = [
     description: 'Travel website where users can view upcoming and past trips. It also has an administration panel for those administrators who are logged in. They can create, delete and modify trips.',
     repoUrl: 'https://github.com/diegoOG09/travels',
     websiteUrl: 'https://viajes-bernal.netlify.app/'
+  },
+  {
+    image: '/assets/screenshots/vue-poke-app.png',
+    title: 'Vue Pokemon App',
+    description: 'Guess the pokemon game',
+    repoUrl: 'https://github.com/diegoOG09/vue-pokeApi',
+    websiteUrl: 'https://vue-poke-apimon.netlify.app/'
+  },
+  {
+    image: '/assets/screenshots/yes-no-app.png',
+    title: 'Yes/No App',
+    description: 'An app that responds Yes or No to a question',
+    repoUrl: '',
+    websiteUrl: 'https://playful-flan-aba5cd.netlify.app/'
   }
 ]
 
